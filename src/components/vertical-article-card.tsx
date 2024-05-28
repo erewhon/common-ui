b9c0bd48a02e330a50daa8088c908329fbc43ea7
Steven Byrnes <steven@byrnes.org>
2024-05-28 18:49:31 -0500

    tweak card formatting

--- src/components/vertical-article-card.tsx
+++ src/components/vertical-article-card.tsx
@@ -11,20 +11,20 @@ export function VerticalArticleCard({ postType, article }: Props) {
     <a href={`/${postType}/${article.slug}`}>
       <div
         className={
-          "h-[360px] p-5 border border-gray-700 rounded hover:shadow hover:scale-110 transition-all bg-black hover:bg-gray-600 flex flex-col items-center"
+          "h-96 w-64 p-5 border border-gray-700 rounded hover:shadow hover:scale-110 transition-all bg-black hover:bg-gray-600 flex flex-col items-center"
         }
       >
         <div>
           <img
             src={article.cardImage}
-            className={"w-48 h-48 rounded-xl object-contain"}
+            className={"w-48 h-48 rounded-xl object-cover"}
           />
         </div>
-        <div className={"mt-2 w-64 truncate"}>{article.title}</div>
+        <div className={"mt-2 w-56 truncate mx-auto"}>{article.title}</div>
         <div className={"text-xs text-slate-600"}>
           {formatDate(article.date)}
         </div>
-        <div className={"text-sm w-64 mt-2  line-clamp-3"}>
+        <div className={"text-sm w-56 mt-2 line-clamp-3"}>
           {article.description}
         </div>
       </div>
